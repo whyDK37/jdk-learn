c715617f374aa68bb074a87a9bd1dd0aebca7433
why <wanghuanyu10>
2020-07-23 09:02:17 +0800

    add demo

--- jdk13/src/test/java/util/concurrent/CompletableFutureDemo.java
+++ jdk13/src/test/java/util/concurrent/CompletableFutureDemo.java
@@ -34,7 +34,7 @@ public class CompletableFutureDemo {
     CompletableFuture<String> cfQuery = CompletableFuture.supplyAsync(() -> queryCode("中国石油"));
 
     // cfQuery成功后继续执行下一个任务:
-    CompletableFuture<Double> cfFetch = cfQuery.thenApplyAsync((code) -> fetchPrice(code));
+    CompletableFuture<Double> cfFetch = cfQuery.thenApplyAsync(CompletableFutureDemo::fetchPrice);
     // cfFetch成功后打印结果:
     cfFetch.thenAccept((result) -> {
       System.out.println("price: " + result);
@@ -64,9 +64,7 @@ public class CompletableFutureDemo {
     CompletableFuture<Object> cfFetch = CompletableFuture.anyOf(cfFetchFromSina, cfFetchFrom163);
 
     // 最终结果:
-    cfFetch.thenAccept((result) -> {
-      System.out.println("price: " + result);
-    });
+    cfFetch.thenAccept((result) -> System.out.println("price: " + result));
     // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
     Thread.sleep(200);
   }
