bfe83ea92b9cd724cd8cec99dac15f9e6f69582f
why <@>
2023-05-08 15:17:39 +0800

    feat: add code

--- jdk17/src/main/java/juc/SynchronizedTest.java
+++ jdk17/src/main/java/juc/SynchronizedTest.java
@@ -10,34 +10,35 @@ public class SynchronizedTest {
         why.start();
         mx.start();
     }
-}
 
-class TicketConsumer implements Runnable {
+    static class TicketConsumer implements Runnable {
 
-    private volatile static Integer ticket;
+        private volatile static Integer ticket;
 
-    public TicketConsumer(int ticket) {
-        TicketConsumer.ticket = ticket;
-    }
+        public TicketConsumer(int ticket) {
+            TicketConsumer.ticket = ticket;
+        }
 
-    @Override
-    public void run() {
-        while (true) {
-            System.out.println(Thread.currentThread().getName() + "开始抢第" + ticket + "张票，对象加锁之前：" + System.identityHashCode(ticket));
-            synchronized (ticket) {
-                System.out.println(Thread.currentThread().getName() + "抢到第" + ticket + "张票，成功锁到的对象：" + System.identityHashCode(ticket));
-                if (ticket > 0) {
-                    try {
-                        //模拟抢票延迟
-                        TimeUnit.SECONDS.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println(Thread.currentThread().getName() + "开始抢第" + ticket + "张票，对象加锁之前：" + System.identityHashCode(ticket));
+                synchronized (ticket) {
+                    System.out.println(Thread.currentThread().getName() + "抢到第" + ticket + "张票，成功锁到的对象：" + System.identityHashCode(ticket));
+                    if (ticket > 0) {
+                        try {
+                            //模拟抢票延迟
+                            TimeUnit.SECONDS.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread().getName() + "抢到了第" + ticket-- + "张票，票数减一");
+                    } else {
+                        return;
                     }
-                    System.out.println(Thread.currentThread().getName() + "抢到了第" + ticket-- + "张票，票数减一");
-                } else {
-                    return;
                 }
             }
         }
     }
 }
+
