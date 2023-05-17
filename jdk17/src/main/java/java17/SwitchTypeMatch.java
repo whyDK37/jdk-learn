cc6a72a7a2ae341860822b0cc43eefd20e9dc372
why <@>
2023-05-17 17:19:41 +0800

    feat: add code

--- jdk17/src/main/java/java17/SwitchTypeMatch.java
+++ jdk17/src/main/java/java17/SwitchTypeMatch.java
@@ -5,6 +5,12 @@ public class SwitchTypeMatch {
   public static void main(String[] args) {
 
     testFooBar(null);
+
+    System.out.println(formatterPatternSwitch(1));
+    System.out.println(formatterPatternSwitch(1L));
+    System.out.println(formatterPatternSwitch(1.1));
+    System.out.println(formatterPatternSwitch("1.1"));
+    System.out.println(formatterPatternSwitch(null));
   }
 
   static String formatterPatternSwitch(Object o) {
@@ -13,12 +19,13 @@ public class SwitchTypeMatch {
       case Long l    -> String.format("long %d", l);
       case Double d  -> String.format("double %f", d);
       case String s  -> String.format("String %s", s);
+      case null -> String.format("null");
       default        -> o.toString();
     };
   }
   static void testFooBar(String s) {
     switch (s) {
-      case null         -> System.out.println("Oops");
+      case null         -> System.out.println("null");
       case "Foo", "Bar" -> System.out.println("Great");
       default           -> System.out.println("Ok");
     }
---
+++ jdk17/src/main/java/juc/LinkedBlockingQueueDemo.java
@@ -0,0 +1,10 @@
+package juc;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LinkedBlockingQueueDemo {
+
+    public static void main(String[] args) {
+        LinkedBlockingQueue<Object> qu = new LinkedBlockingQueue<>();
+    }
+}
